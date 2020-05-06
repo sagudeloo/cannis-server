c3850445032be06fe9b90e6d0f3eaba2b9ccd86c
sagudeloo <sagudeloo@eafit.edu.co>
2020-05-05 21:49:01 -0500

    convitiendo la app de electron en una app web

--- src/index.py
+++ src/index.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'INDEX'
+    return render_template('index.html')
+
+@app.route('/anadirObjeto', methods=['POST', 'GET'])
+def anadirObjeto():
+    if request.method == 'POST':
+        print(request.form.keys)
+    return render_template('nuevoObjeto.html')
+
+@app.route('/buscarObjetos', methods=['POST', 'GET'])
+def buscarObjetos():
+    if request.method == 'POST':
+        print(request.form.keys)
+    return render_template('buscarObjetos.html')
+
+@app.route('/login')
+def login():
+    return 'login'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port = 3000, debug=True)
